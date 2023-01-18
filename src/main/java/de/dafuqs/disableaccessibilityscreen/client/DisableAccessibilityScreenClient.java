4378f502e234042f28d6779c6eab38df1be4bfe1
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-01-18 21:55:21 +0100

    Initial commit

---
+++ src/main/java/de/dafuqs/disableaccessibilityscreen/client/DisableAccessibilityScreenClient.java
@@ -0,0 +1,11 @@
+package de.dafuqs.disableaccessibilityscreen.client;
+
+import net.fabricmc.api.*;
+
+@Environment(EnvType.CLIENT)
+public class DisableAccessibilityScreenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
