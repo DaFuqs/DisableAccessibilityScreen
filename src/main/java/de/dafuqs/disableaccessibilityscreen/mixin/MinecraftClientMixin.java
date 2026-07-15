6f901c2b308fef927613a434c2a819d08957fe6e
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-07-15 22:05:25 +0200

    Ported to MC 26.2

--- src/main/java/de/dafuqs/disableaccessibilityscreen/mixin/MinecraftClientMixin.java
+++ src/main/java/de/dafuqs/disableaccessibilityscreen/mixin/MinecraftClientMixin.java
@@ -7,11 +7,11 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftClientMixin {
-    
+
     @Shadow @Final public Options options;
-    
-    @Inject(method = "addInitialScreens(Ljava/util/List;)Z", at = @At("HEAD"))
-    private void disableAccessibilityScreen(CallbackInfoReturnable<Runnable> cir) {
+
+    @Inject(method = "onResourceLoadFinished(Lnet/minecraft/client/GameLoadCookie;)V", at = @At("HEAD"))
+    private void disableAccessibilityScreen(GameLoadCookie loadCookie, CallbackInfo ci) {
         this.options.onboardAccessibility = false;
     }
 
